975b2fdd01c793158d5973ba7366f30f5d2ea5fa
Wireless Dan <wirelessdan@proton.me>
2023-12-07 18:32:53 -0500

    Compiles and runs with no issues.  Added README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+to compile run:
+
+go build
+
+to run run:
+
+go run go-server
--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
-import(
-	"fmt"
-	"log"
-	"net/http"
+import (
+	"fmt"      // I/O functions
+	"log"      // used to log stuff, in this case we use a function called Fatal
+	"net/http" // http funcionality
 )
 
-func formHandler(w, http.ResponseWriter, r *http.Request){
+func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
-		fmr.Fprintf(w,"ParseForm() err: %v", err)  //"Fprintf is just basically printing it on the screen"
+		fmt.Fprintf(w, "ParseForm() err: %v", err) //"Fprintf is just basically printing it on the screen"
 		return
 	}
 	fmt.Fprintf(w, "POST request succussful.")
@@ -18,29 +18,27 @@ func formHandler(w, http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Address = %s\n", address)
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request){
-if r.URL.Path != "/hello" {
-	http.Error(w, "404 not found", http.StatusNotFound)
-	return
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
 
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
 }
-if r.Method != "GET"{
-	http.Error(w, "method is not supported", http.StatusNotFound)
-	return
-}
-}
-fmt.FPrintf(w, "hello!")
 
-
-func main(){
+func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
-
 	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err !=nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
