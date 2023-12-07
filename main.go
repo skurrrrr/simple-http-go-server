a61beb5145e026bf4ede8f5d21b555c8a45020eb
Wireless Dan <wirelessdan@proton.me>
2023-12-07 17:26:14 -0500

    outlined some funcionality

--- main.go
+++ main.go
@@ -6,6 +6,19 @@ import(
 	"net/http"
 )
 
-func main(
+funchelloHandler(w http.ResponseWriter, r *http.Request){
 	
-)
+}
+
+func main(){
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err !=nil {
+		log.Fatal(err)
+	}
+}
